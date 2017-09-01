8854d7d49b1bfaefb6c36de21a2d16b046055978
TungTB <tranbatungbk@gmail.com>
2017-09-01 11:38:15 +0700

    For Docker

--- builddocker.sh
+++ builddocker.sh
@@ -1,4 +1,4 @@
 sh builddist.sh
 dateVersion=$(date +"%Y%m%d")
-docker build -t tranbatungbk/angular2-cli-starter:$dateVersion .
-docker push tranbatungbk/angular2-cli-starter:$dateVersion
+docker build -t tungtb/angular2-cli-starter:$dateVersion .
+docker push tungtb/angular2-cli-starter:$dateVersion
